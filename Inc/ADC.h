930dfab99910f209c606764f3256c42778259452
martinkresta <86981335+martinkresta@users.noreply.github.com>
2022-03-02 20:08:06 +0100

    Added module ADC

---
+++ Inc/ADC.h
@@ -0,0 +1,24 @@
+/*
+ * ADC.h
+ *
+ *  Created on: Jul 25, 2021
+ *      Author: Martin
+ */
+
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+
+
+#include "main.h"
+
+
+#define ADC_VREF_MV					3313   // mV
+
+void ADC_Init(ADC_HandleTypeDef*  ADC, DMA_HandleTypeDef* DMA, uint8_t numOfChannels);
+
+void ADC_StartConversion(void);
+uint16_t ADC_GetValue(uint8_t channel);
+
+
+
+#endif /* INC_ADC_H_ */
---
+++ Src/ADC.c
@@ -0,0 +1,55 @@
+/*
+ * ADC.c
+ *
+ *  Created on: Jul 25, 2021
+ *      Author: Martin
+ */
+
+#include "main.h"
+#include "adc.h"
+
+
+ADC_HandleTypeDef* adc;
+DMA_HandleTypeDef* dma;
+
+uint8_t mNumOfChannels;
+uint16_t* mResults = NULL;
+
+
+void ADC_Init(ADC_HandleTypeDef*  ADC, DMA_HandleTypeDef* DMA, uint8_t numOfChannels)
+{
+	adc = ADC;
+	dma = DMA;
+	mNumOfChannels = numOfChannels;
+	HAL_ADCEx_Calibration_Start(adc, ADC_SINGLE_ENDED);
+	mResults = malloc(mNumOfChannels * sizeof(uint16_t));
+}
+
+
+void ADC_StartConversion(void)
+{
+	if (mResults != NULL)
+	{
+		HAL_ADC_Start_DMA(adc, (uint32_t*)mResults, mNumOfChannels);
+	}
+}
+
+uint16_t ADC_GetValue(uint8_t channel)
+{
+	if (channel < mNumOfChannels && mResults != NULL)
+	{
+		return mResults[channel];
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+
+	HAL_ADC_Stop_DMA(adc);
+
+}
